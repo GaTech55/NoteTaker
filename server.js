84ae9c5b2a8445ec0f6f0c37afd14cd7bb719bea
David Brown <gatech55@gmail.com>
2020-11-21 15:59:20 -0500

    Working on understanding the needed Store files

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
-
 const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+var logger = require("morgan");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -14,24 +16,18 @@ const express = require("express");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8089;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(logger("dev"));
+app.use(express.static("public"));
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
-
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
